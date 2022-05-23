ae3b033dfa085b89e890c6e72b12922667da9e14
LFBarreto <lfb.access@gmail.com>
2022-05-23 16:14:23 +0200

    LIVE-2238 - LLM - LLD - Learn webview uri from remote config

--- apps/ledger-live-mobile/src/screens/Learn/index.tsx
+++ apps/ledger-live-mobile/src/screens/Learn/index.tsx
@@ -5,15 +5,12 @@ import { URLSearchParams } from "react-native-url-polyfill";
 import styled, { useTheme } from "styled-components/native";
 import { useTranslation } from "react-i18next";
 import NetInfo from "@react-native-community/netinfo";
-import useEnv from "@ledgerhq/live-common/lib/hooks/useEnv";
 import { useNavigation } from "@react-navigation/native";
+import useFeature from "@ledgerhq/live-common/lib/featureFlags/useFeature";
 import extraStatusBarPadding from "../../logic/extraStatusBarPadding";
 import LoadingView from "./LoadingScreen";
 import NoConnectionErrorScreen from "./NoConnectionErrorScreen";
 import { Track } from "../../analytics";
-
-const learnProdURL = "https://www.ledger.com/ledger-live-learn";
-const learnStagingURL = "https://www-ppr.ledger.com/ledger-live-learn";
 
 const SafeContainer = styled(SafeAreaView)`
   flex: 1;
@@ -25,15 +22,20 @@ const StyledWebview = styled(WebView)`
   background-color: transparent; // avoids white background before page loads
 `;
 
+const DEFAULT_LEARN_URL = "https://www.ledger.com/ledger-live-learn";
+
 function Learn() {
   const { i18n } = useTranslation();
+  const learn = useFeature("learn");
   const navigation = useNavigation();
   const [canGoBack, setCanGoBack] = useState(true);
   const {
     colors: { type: themeType },
   } = useTheme();
 
-  const useStagingURL = useEnv("USE_LEARN_STAGING_URL");
+  const learnURL = learn?.params?.mobile?.url
+    ? learn.params.mobile.url
+    : DEFAULT_LEARN_URL;
   const ref = useRef<WebView>(null);
 
   const params = new URLSearchParams({
@@ -41,9 +43,7 @@ function Learn() {
     lang: i18n.languages[0],
   });
 
-  const uri = `${
-    useStagingURL ? learnStagingURL : learnProdURL
-  }?${params.toString()}`;
+  const uri = `${learnURL}?${params.toString()}`;
 
   const [initialLoadingDone, setInitialLoadingDone] = useState(false);
   const [loading, setLoading] = useState(false);
